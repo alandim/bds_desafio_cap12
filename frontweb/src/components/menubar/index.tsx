bfcfe0331c02e8ed0b080c97718773f5a02e5da4
Andre Pereira <andre.pereira@mprs.mp.br>
2024-03-29 10:33:09 -0300

    grafico e filtro

---
+++ frontweb/src/components/menubar/index.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+function MenuBar() {
+  return (
+    <header className="main-header-container">
+      <h1 className="main-header-title">BDS Vendas - Trabalho Final Capítulo 12</h1>
+    </header>
+  );
+}
+
+export default MenuBar;
